6fc217f1c87fe53a5ef7ae31033924b73f83d9e6
Ekene Nkem-Mmekam <ekenayy@gmail.com>
2025-02-10 23:23:03 -0700

    revert changes

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -9,4 +9,34 @@ export default defineConfig({
     react(),
     crx({ manifest }),
   ],
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: 'index.html',
+  //       background: 'src/background/background.ts',
+  //       content: 'src/content/content.tsx',
+  //       options: 'src/optionsPage/optionsPage.html'
+  //     },
+  //     output: {
+  //       entryFileNames: (chunk) => {
+  //         if (chunk.name === 'background') {
+  //           return 'src/background/background.js';
+  //         }
+  //         if (chunk.name === 'content') {
+  //           return 'src/content/content.js';
+  //         }
+  //         return '[name].js';
+  //       },
+  //       chunkFileNames: 'assets/[name].[hash].js',
+  //       assetFileNames: 'assets/[name].[ext]'
+  //     },
+  //   },
+  // },
+  // server: {
+  //   port: 5173,
+  //   strictPort: true,
+  //   hmr: {
+  //     port: 5173,
+  //   },
+  // },
 })
