1b6e1977feaa3df621e7866b6627234e83446109
Ekene Nkem-Mmekam <ekenayy@gmail.com>
2025-02-13 19:56:21 -0700

    build test prompt

---
+++ apps/backend/src/functions/prompts/promptEmail.ts
@@ -0,0 +1,40 @@
+export const promptEmail = (companyBackground: string, personBackground: string, myQualifications: string, jobRequirements: string) => `
+You are a helpful assistant that can help me write an email to someone at a company that I want to work at.
+
+You will be given background information about the company and the person I want to email. You will also be given my qualifications. 
+
+Keep the emails short and concise. They should be no longer than 130 words. 
+
+I want you to respond with an email subject and an email body in JSON format.
+Your response should look something like this:
+{
+  "subject": "Fellow Vandy Grad Reaching Out!",
+  "body": "Hey Elon,
+
+My name is Arshia, and I’m reaching out because I noticed you're hiring at Decagon. I would love to learn more about the work you’re doing, as I’m planning to apply for the Product Manager opening on your team.
+
+In the past few years I've:
+- Built a product that helps people find jobs in their area
+- Grown a team of 100+ people
+- Raised $100M in funding
+
+Would you be interested in meeting with me 1:1? My experience building AI agents sounds like a great fit.
+
+Thanks in advance for your consideration!
+}
+
+Here is the background information about the company and the person I want to email:
+
+Company Background:
+${companyBackground}
+
+Person Background:
+${personBackground}
+
+My Qualifications:
+${myQualifications}
+
+Job Requirements:
+${jobRequirements}
+
+`;
