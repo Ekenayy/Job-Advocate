3944d9c7afc3cf68b8e935f2ed599b03ef9265f2
Ekene Nkem-Mmekam <ekenayy@gmail.com>
2025-02-17 10:55:02 -0700

    add autocorrect

--- apps/frontend/src/components/Adovcate.tsx
+++ apps/frontend/src/components/Adovcate.tsx
@@ -53,6 +53,7 @@ const Advocate: React.FC<AdvocateProps> = ({ name, title, company, initials, lin
           onChange={(e) => setEmailSubject(e.target.value)}
           placeholder="Your Subject here.."
           className="w-full p-2 border rounded-md bg-white"
+          autoCorrect="on"
           minRows={1}
         />}
         {isLoadingEmail ? <Skeleton className="w-full p-2 rounded-md border h-[500px]"  /> : <TextareaAutosize
