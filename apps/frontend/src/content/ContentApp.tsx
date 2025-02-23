5c3fc279450c29205529c2580c34711e1dd799f6
Ekene Nkem-Mmekam <ekenayy@gmail.com>
2025-02-22 21:52:28 -0700

    remove useEffect

--- apps/frontend/src/content/ContentApp.tsx
+++ apps/frontend/src/content/ContentApp.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Advocate from "../components/Advocate";
 import { GmailService } from '../services/gmailService';
 import { useUser } from '../context/UserProvder';
