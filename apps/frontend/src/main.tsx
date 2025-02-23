b48a4a8b2e75cd2fa4889df4a31324531765b193
Ekene Nkem-Mmekam <ekenayy@gmail.com>
2025-02-22 17:36:31 -0700

    init clerk

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,12 +3,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { UserProvider } from './context/UserProvder.tsx'
+import { ClerkProvider } from '@clerk/chrome-extension'
+
+if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </ClerkProvider>
   </React.StrictMode>
 )
